63cf481217da3e268a7630448259fb6de6642496
Mercy Mwikali <56218360+mercymwikali@users.noreply.github.com>
2024-04-15 20:51:51 +0300

    Update apiSlice.js

--- Client/src/app/api/apiSlice.js
+++ Client/src/app/api/apiSlice.js
@@ -1,7 +1,7 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-    baseQuery: fetchBaseQuery({baseUrl:'https://crowwriter-api.vercel.app/', credentials: 'include'}),
+    baseQuery: fetchBaseQuery({baseUrl:'https://crowwriter-api.vercel.app', credentials: 'include'}),
     tagTypes: ['Order', 'Users'],
     endpoints: (builder) => ({
         
